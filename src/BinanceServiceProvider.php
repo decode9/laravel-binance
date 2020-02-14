c5afb8fc0835b836e5e760a1bddf02553728f93e
Jorge Bastidas <jorgebastidas9@gmail.com>
2020-02-13 21:39:23 -0400

    fix

--- src/BinanceServiceProvider.php
+++ src/BinanceServiceProvider.php
@@ -11,17 +11,15 @@ class BinanceServiceProvider extends ServiceProvider {
 	{
 		$this->publishes([
 			__DIR__.'/../config/binance.php' => config_path('binance.php')
-		]);
+		], 'binance');
+		$this->app->bind('binance', function() {
+			return new BinanceAPI();
+		});
 	} // boot
 
 	public function register() 
 	{
 		$this->mergeConfigFrom(__DIR__.'/../config/binance.php', 'binance');
-		$this->app->bind('binance', function() {
-			return new BinanceAPI(config('binance'));
-		});
-
-		
 
 	} // register
 }
